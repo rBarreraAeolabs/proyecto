a52153f2e5c5a42c5bf2bbc8d11587047eb68528
Ifyum <ifyumx@gmail.com>
2019-10-23 02:06:17 -0300

    arreglos plazos

--- app/src/main/java/com/gruposolux/rcivil/pdisciplinario/service/FechasService.java
+++ app/src/main/java/com/gruposolux/rcivil/pdisciplinario/service/FechasService.java
@@ -14,49 +14,7 @@ import java.util.List;
 
 @Service("fechasService")
 public class FechasService {
-//
-//    public int diasHabiles(Calendar fechaInicial, Calendar fechaFinal, List<Date> listaFechasNoLaborables) {
-//        int diffDays = 0;
-//        boolean diaHabil = false;
-//        //mientras la fecha inicial sea menor o igual que la fecha final se cuentan los dias
-//        while (fechaInicial.before(fechaFinal) || fechaInicial.equals(fechaFinal)) {
-//
-//            if (!listaFechasNoLaborables.isEmpty()) {
-//                for (Date date : listaFechasNoLaborables) {
-//                    Date fechaNoLaborablecalendar = fechaInicial.getTime();
-//                    //si el dia de la semana de la fecha minima es diferente de sabado o domingo
-//                    if (fechaInicial.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY && fechaInicial.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && !fechaNoLaborablecalendar.equals(date)) {
-//                        //se aumentan los dias de diferencia entre min y max
-//                        diaHabil = true;
-//                    } else {
-//                        diaHabil = false;
-//                        break;
-//                    }
-//                }
-//            } else {
-//                if (fechaInicial.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY && fechaInicial.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY) {
-//                    //se aumentan los dias de diferencia entre min y max
-//                    diffDays++;
-//                }
-//            }
-//            if (diaHabil == true) {
-//                diffDays++;
-//            }
-//            //se suma 1 dia para hacer la validacion del siguiente dia.
-//            fechaInicial.add(Calendar.DATE, 1);
-//        }
-//        return diffDays;
-//    }
 
-
-
-
-    public Date  sumarRestarDias(Date fecha, int dias){
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(fecha);
-        calendar.add(Calendar.DAY_OF_YEAR,dias);
-        return  calendar.getTime();
-    }
 
     public Date  calcularFecha(Date fecha, int dias,List<Date> listaFechasNoLaborables){
 //        Calendar calendar = Calendar.getInstance();
